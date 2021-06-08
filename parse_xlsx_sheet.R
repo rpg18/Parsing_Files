464087e28817901694bbe30587297c271c6c2670
rpg18 <49445224+rpg18@users.noreply.github.com>
2021-06-08 16:24:11 +0100

    Update parse_xlsx_sheet.R

--- parse_xlsx_sheet.R
+++ parse_xlsx_sheet.R
@@ -1,10 +1,10 @@
 ## Script to parse xlsx files
 # extracts observations and variable names by selecting specific values of interest
-# creates csv files for each value of interested
-# e.g., value==10, it creates csv files to store all variable and observation names with value==10.
+# e.g., when values <- 10, it will create a csv file which stores all variable and observation names with values==10
 
 # load packages
-library(openxlsx) # to install it: install.packages("openxlsx", dependencies = TRUE) # just run once
+# install.packages("openxlsx", dependencies = TRUE) # uncomment
+library(openxlsx) 
 
 # input parameters
 workingdir <- ''
@@ -12,20 +12,20 @@ setwd(workingdir)
 filename <- file.path('.xlsx')
 sheetname <- ''
 outfile <- ''
-values <- c() # enter values of interest, e.g., values <- c(0,5,10)
+values <- c() # enter values of interest e.g., values <- c(0,5,10)
 
 # function that parses the file by looking for selected values, and writes a csv output file
 parsing <- function(values){
   for (i in seq_along(values)){
     # read input file xlsx
     file <- read.xlsx(filename, sheet = sheetname, rowNames = T)
-    # search for values of interest and generate list of lists, one list per protein
+    # search for values of interest and generate list of lists, one list per protein/variable
     outlist <- lapply(file,function(x) {x == values[i]})
-    # function to convert unwanted values to NA values, it keeps sample names of interested values
+    # nested function to convert unwanted values into NA
     def <- function(cellvalue){
       ifelse(cellvalue == T, rownames(file), NA)
     }
-    # apply function to convert sample names, and keep interested sample names, if value is = to selected value
+    # apply function to convert sample names, and keep interested sample names when value is = to selected value
     outlist <- lapply(outlist,def)
     # remove NA values from list of lists
     outlist <- lapply(outlist,function(x) {x[!is.na(x)]})
