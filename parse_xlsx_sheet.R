cac0cb3fbdd5609a1dfc58da0eae84ef437927ea
rpg18 <49445224+rpg18@users.noreply.github.com>
2021-06-08 16:12:46 +0100

    Update parse_xlsx_sheet.R

--- parse_xlsx_sheet.R
+++ parse_xlsx_sheet.R
@@ -1,5 +1,7 @@
-#### Script to parse xlsx files: extracts observations and variables names by selecting specific values of interest.
-### Output: creates csv files for each value of interested, such as: value==10, it will create csv files to store all variable and observation names with value==10.
+## Script to parse xlsx files
+# extracts observations and variable names by selecting specific values of interest
+# creates csv files for each value of interested
+# e.g., value==10, it creates csv files to store all variable and observation names with value==10.
 
 # load packages
 library(openxlsx) # to install it: install.packages("openxlsx", dependencies = TRUE) # just run once
@@ -10,7 +12,7 @@ setwd(workingdir)
 filename <- file.path('.xlsx')
 sheetname <- ''
 outfile <- ''
-values <- c() # please, enter values of interest, such as: c(0,5,10)
+values <- c() # enter values of interest, e.g., values <- c(0,5,10)
 
 # function that parses the file by looking for selected values, and writes a csv output file
 parsing <- function(values){
@@ -19,16 +21,15 @@ parsing <- function(values){
     file <- read.xlsx(filename, sheet = sheetname, rowNames = T)
     # search for values of interest and generate list of lists, one list per protein
     outlist <- lapply(file,function(x) {x == values[i]})
-    # function to convert non-interested values to NA values, 
-    # and keep sample names of interested values
+    # function to convert unwanted values to NA values, it keeps sample names of interested values
     def <- function(cellvalue){
       ifelse(cellvalue == T, rownames(file), NA)
     }
     # apply function to convert sample names, and keep interested sample names, if value is = to selected value
     outlist <- lapply(outlist,def)
     # remove NA values from list of lists
-    outlist <- lapply(outlist,function(x) x[!is.na(x)])
-    # from list to pretty list table
+    outlist <- lapply(outlist,function(x) {x[!is.na(x)]})
+    # from list to formatted list table
     outfinal <- format(outlist)
     # write csv output file
     write.table(outfinal,paste0(outfile,values[i],".csv"), sep = ',',col.names = F)
